fadaa0182f1a026e78a481ad01538262682db71e
Tijn <tijn.stuifbergen@hva.nl>
2023-09-11 12:48:33 +0200

    Movement player

--- game/sketch.js
+++ game/sketch.js
@@ -1,21 +1,32 @@
-var moveLeft = false
-var moveRight = false
+var playerShip;
 
 function setup() {
   createCanvas(1200, 600);
+  playerShip = new Player();;
+}
+
+function Player() {
+  this.x = 1200/2 - 30;
+  this.y = 600-30;
+  this.color = "green";
+  this.width = 60;
+  this.height = 20;
+}
+
+this.spawn = function() {
+  fill(0,255,0);
+  noStroke();
+  rect(playerShip.x, playerShip.y, playerShip.width, playerShip.height);
+  console.log(playerShip.x, playerShip.y, playerShip.width, playerShip.height)
 }
 
 function draw() {
-  noStroke();
   background(0);
-  rect(width / 2 - 30, height - 30, 60, 20)
-}
-
-function keyPressed() {
-  if ( Keycode === LEFT_ARROW) {
-    moveLeft = true;
+  this.spawn();
+  if (keyIsDown(LEFT_ARROW)) {
+    playerShip.x = playerShip.x - 8
   }
-  if (Keycode === RIGHT_ARROW) {
-    moveRight = true;
+  if (keyIsDown(RIGHT_ARROW)) {
+    playerShip.x = playerShip.x + 8
   }
 }

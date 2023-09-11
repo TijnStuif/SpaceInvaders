b9185c0270800f217926bc21832d9b1f1d6c846d
Tijn <tijn.stuifbergen@hva.nl>
2023-09-11 10:07:41 +0200

    Create player and margin

--- game/sketch.js
+++ game/sketch.js
@@ -1,7 +1,21 @@
+var moveLeft = false
+var moveRight = false
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1200, 600);
 }
 
 function draw() {
-  background(220);
+  noStroke();
+  background(0);
+  rect(width / 2 - 30, height - 30, 60, 20)
+}
+
+function keyPressed() {
+  if ( Keycode === LEFT_ARROW) {
+    moveLeft = true;
+  }
+  if (Keycode === RIGHT_ARROW) {
+    moveRight = true;
+  }
 }

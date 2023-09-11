29d449b8ffd7d2f37f18ec2a41567eacc207f799
Tijn <tijn.stuifbergen@hva.nl>
2023-09-11 15:01:50 +0200

    Player Ship more detailed

--- game/sketch.js
+++ game/sketch.js
@@ -3,30 +3,37 @@ var playerShip;
 function setup() {
   createCanvas(1200, 600);
   playerShip = new Player();;
+  playerBullet = new pBullet();;
 }
 
 function Player() {
-  this.x = 1200/2 - 30;
-  this.y = 600-30;
-  this.color = "green";
+  this.x = width/2 - 30;
+  this.y = height-30;
   this.width = 60;
   this.height = 20;
 }
 
-this.spawn = function() {
+function pBullet() {
+  this.x = playerShip.x + 30;
+  this.y = playerShip.y - 10;
+  this.width = 10;
+  this.height = 20;
+}
+
+Player.spawn = function() {
   fill(0,255,0);
   noStroke();
   rect(playerShip.x, playerShip.y, playerShip.width, playerShip.height);
-  console.log(playerShip.x, playerShip.y, playerShip.width, playerShip.height)
+  rect(playerShip.x + 20, playerShip.y-10,playerShip.width - 40, playerShip.height - 10);
 }
 
 function draw() {
   background(0);
-  this.spawn();
-  if (keyIsDown(LEFT_ARROW)) {
-    playerShip.x = playerShip.x - 8
+  Player.spawn();
+  if (keyIsDown(65)  && playerShip.x > 0 || keyIsDown(LEFT_ARROW) && playerShip.x > 0) {
+    playerShip.x = playerShip.x - 10
   }
-  if (keyIsDown(RIGHT_ARROW)) {
-    playerShip.x = playerShip.x + 8
+  if (keyIsDown(68) && playerShip.x < 1140 || keyIsDown(RIGHT_ARROW) && playerShip.x < 1140) {
+    playerShip.x = playerShip.x + 10
   }
 }

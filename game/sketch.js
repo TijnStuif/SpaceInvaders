b71d1f55a988c1e6fcc551a95304733ee4676570
Tijn <tijn.stuifbergen@hva.nl>
2023-09-11 16:06:43 +0200

    Bullet

--- game/sketch.js
+++ game/sketch.js
@@ -14,10 +14,11 @@ function Player() {
 }
 
 function pBullet() {
-  this.x = playerShip.x + 30;
+  this.x = playerShip.x + 125;
   this.y = playerShip.y - 10;
   this.width = 10;
   this.height = 20;
+  this.pBulletFired = false;
 }
 
 Player.spawn = function() {
@@ -25,6 +26,19 @@ Player.spawn = function() {
   noStroke();
   rect(playerShip.x, playerShip.y, playerShip.width, playerShip.height);
   rect(playerShip.x + 20, playerShip.y-10,playerShip.width - 40, playerShip.height - 10);
+}
+
+pBullet.spawn = function() {
+  fill(127);
+  stroke(150);
+  rect(playerBullet.x, playerBullet.y, playerBullet.width, playerBullet.height);
+  console.log(playerBullet.x, playerBullet.y, playerBullet.width, playerBullet.height)
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    pBullet.pBulletFired = true;
+  }
 }
 
 function draw() {
@@ -35,5 +49,12 @@ function draw() {
   }
   if (keyIsDown(68) && playerShip.x < 1140 || keyIsDown(RIGHT_ARROW) && playerShip.x < 1140) {
     playerShip.x = playerShip.x + 10
+  }
+  if (pBullet.pBulletFired == true) {
+      pBullet.spawn();
+      playerBullet.y = playerBullet.y - 5;
+  }
+  if (playerBullet.y < 0) {
+    pBullet.pBulletFired == false;
   }
 }

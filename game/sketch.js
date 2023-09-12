1ee03e3a53d47bdedc33346a2ef4e425286d4ce6
Tijn <tijn.stuifbergen@hva.nl>
2023-09-12 10:31:31 +0200

    movement optie met wasd

--- game/sketch.js
+++ game/sketch.js
@@ -17,10 +17,10 @@ function draw () {
   fill(0,255,0);
   noStroke();
   rect(playerShipX, playerShipY, widthPlayerShip, heightPlayerShip)
-  if (keyIsDown(LEFT_ARROW) && playerShipX > 0){
+  if (keyIsDown(LEFT_ARROW) && playerShipX > 0 || keyIsDown(65) && playerShipX > 0){
     playerShipX = playerShipX - 10;
   }
-  if (keyIsDown(RIGHT_ARROW) && playerShipX < 1140) {
+  if (keyIsDown(RIGHT_ARROW) && playerShipX < 1140 || keyIsDown(68) && playerShipX < 1140) {
     playerShipX = playerShipX + 10
   }
 }

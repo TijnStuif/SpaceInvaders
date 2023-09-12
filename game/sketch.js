8e482705a3c6360482cc8c4821948fd9532e4909
Tijn <tijn.stuifbergen@hva.nl>
2023-09-12 09:01:17 +0200

    movement

--- game/sketch.js
+++ game/sketch.js
@@ -1,17 +1,21 @@
 const widthPlayerShip = 60
 const heightPlayerShip = 20
+var playerShipX = 570
+const playerShipY = 570
 
 function setup () {
   createCanvas(1200,600);
-}
-
-function Player() {
-
 }
 
 function draw () {
   background(0);
   fill(0,255,0);
   noStroke();
-  rect(width / 2 - 30, height - 30, widthPlayerShip, heightPlayerShip)
+  rect(playerShipX, playerShipY, widthPlayerShip, heightPlayerShip)
+  if (keyIsDown(LEFT_ARROW) && playerShipX > 0){
+    playerShipX = playerShipX - 10;
+  }
+  if (keyIsDown(RIGHT_ARROW) && playerShipX < 1140) {
+    playerShipX = playerShipX + 10
+  }
 }

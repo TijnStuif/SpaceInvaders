e46fcbcc60e196e0db6d4fd97d98d5dcbb6ed61f
Tijn <tijn.stuifbergen@hva.nl>
2023-09-14 10:53:24 +0200

    png loaded onto the player ship

--- game/sketch.js
+++ game/sketch.js
@@ -1,12 +1,15 @@
 const widthPlayerShip = 60;
 const heightPlayerShip = 20;
-var playerShipX = 570;
-const playerShipY = 570;
+var playerShipX = 565;
+const playerShipY = 560;
 var playerBulletX = 595;
 var playerBulletY = 580;
+var enemyShipX = 100;
+var enemyShipY = 50;
 
 function preload () {
   spaceImg = loadImage('images_game/space.jpg')
+  playerShipImg = loadImage('images_game/player_ship.png')
 }
 function setup () {
   createCanvas(1200,600);
@@ -14,9 +17,10 @@ function setup () {
 
 function draw () {
   image(spaceImg, 0, 0);
-  fill(0,255,0);
-  noStroke();
-  rect(playerShipX, playerShipY, widthPlayerShip, heightPlayerShip);
+  image(playerShipImg, playerShipX, playerShipY);
+  /*for (let enemyShip = 100; enemyShip < 200; enemyShip++) {
+    enemyShip(100 + enemyShipX * 200, 0, 100 + enemyShipX * 200, enemyShipY)
+  } */
   if (keyIsDown(LEFT_ARROW) && playerShipX > 0 || keyIsDown(65) && playerShipX > 0){
     playerShipX = playerShipX - 10;
   }

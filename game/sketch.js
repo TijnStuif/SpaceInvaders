6dbfd3d299364867e82b05f30305a9d7f04c60dc
Tijn <tijn.stuifbergen@hva.nl>
2023-09-14 12:06:39 +0200

    mystery numbers removed, better documentation

--- game/sketch.js
+++ game/sketch.js
@@ -1,6 +1,9 @@
 const widthPlayerShip = 60;
 const heightPlayerShip = 20;
-var playerShipX = 565;
+const moveSpeedPlayerShip = 10;
+const distanceFromLeftSide = 5;
+const distanceFromRightSide = 65;
+var playerShipX = 568;
 const playerShipY = 560;
 var playerBulletX = 595;
 var playerBulletY = 580;
@@ -8,8 +11,8 @@ var enemyShipX = 100;
 var enemyShipY = 50;
 
 function preload () {
-  spaceImg = loadImage('images_game/space.jpg')
-  playerShipImg = loadImage('images_game/player_ship.png')
+  spaceImg = loadImage('images_game/space.jpg');
+  playerShipImg = loadImage('images_game/player_ship.png');
 }
 function setup () {
   createCanvas(1200,600);
@@ -18,13 +21,15 @@ function setup () {
 function draw () {
   image(spaceImg, 0, 0);
   image(playerShipImg, playerShipX, playerShipY);
-  /*for (let enemyShip = 100; enemyShip < 200; enemyShip++) {
-    enemyShip(100 + enemyShipX * 200, 0, 100 + enemyShipX * 200, enemyShipY)
-  } */
-  if (keyIsDown(LEFT_ARROW) && playerShipX > 0 || keyIsDown(65) && playerShipX > 0){
-    playerShipX = playerShipX - 10;
+  for (let enemyShipX = 100; enemyShipX < 1200; enemyShipX++) {for (let enemyShipY = 50; enemyShipY < 150; enemyShipY++)
+    rect(100 + enemyShipX * 200, 0, 100 + enemyShipX * 200, enemyShipY)
   }
-  if (keyIsDown(RIGHT_ARROW) && playerShipX < 1140 || keyIsDown(68) && playerShipX < 1140) {
-    playerShipX = playerShipX + 10
+  console.log(enemyShipX)
+  console.log(enemyShipY)
+  if (keyIsDown(LEFT_ARROW) && playerShipX > distanceFromLeftSide || keyIsDown(65) && playerShipX > distanceFromLeftSide){
+    playerShipX = playerShipX - moveSpeedPlayerShip;
+  }
+  if (keyIsDown(RIGHT_ARROW) && playerShipX < width - distanceFromRightSide || keyIsDown(68) && playerShipX < width - distanceFromRightSide) {
+    playerShipX = playerShipX + moveSpeedPlayerShip;
   }
 }

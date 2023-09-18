8d5ce57ddb419c44c49da7f4af3765ec06caf674
Tijn <tijn.stuifbergen@hva.nl>
2023-09-18 12:58:35 +0200

    Added .png files for enemies, non-used const gone

--- game/sketch.js
+++ game/sketch.js
@@ -1,7 +1,3 @@
-const widthPlayerShip = 60;
-const heightPlayerShip = 20;
-const widthEnemyShip = 60;
-const heightEnemyShip = 20;
 const moveSpeedPlayerShip = 10;
 const distanceFromLeftSide = 5;
 const distanceFromRightSide = 65;
@@ -17,6 +13,8 @@ let enemyShipY = 50;
 function preload () {
   spaceImg = loadImage('images_game/space.jpg');
   playerShipImg = loadImage('images_game/player_ship.png');
+  peacefulEnemyImg = loadImage('images_game/peaceful_enemy.png');
+  shootingEnemyImg = loadImage('images_game/shooting_enemy.png');
 }
 function setup () {
   createCanvas(1200,600);
@@ -28,12 +26,12 @@ function draw () {
   fill(255, 0, 0);
   for (let enemyShooterX = 0.4; enemyShooterX < 5; enemyShooterX++) {
     for (let enemyShooterY = 0; enemyShooterY < 1; enemyShooterY++) {
-    rect(100 + enemyShooterX * 200, 100 + enemyShooterY * 50, widthEnemyShip, heightEnemyShip) }
+    image(shootingEnemyImg, 100 + enemyShooterX * 200, 100 + enemyShooterY * 50) }
   }
   fill(0, 0, 255);
   for (let enemyShipX = 0.4; enemyShipX < 5; enemyShipX++) {
     for (let enemyShipY = 2; enemyShipY < 5; enemyShipY++)
-    rect(100 + enemyShipX * 200, 50 + enemyShipY * 50, widthEnemyShip, heightEnemyShip)
+    image(peacefulEnemyImg, 100 + enemyShipX * 200, 50 + enemyShipY * 50);
   }
   if (keyIsDown(LEFT_ARROW) && playerShipX > distanceFromLeftSide || keyIsDown(AKeyCode) && playerShipX > distanceFromLeftSide){
     playerShipX = playerShipX - moveSpeedPlayerShip;
